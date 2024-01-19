73b027a2f045b1cb0e2ae45a9cc0af11beed7e71
alloyking1 <alloyking1@gmail.com>
2024-01-19 14:38:37 +0100

    minor changes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -52,27 +52,6 @@
       </div>
     </main>
 
-    <div fixed-plugin>
-      <a fixed-plugin-button class="bottom-7.5 right-7.5 text-xl z-990 shadow-soft-lg rounded-circle fixed cursor-pointer bg-white px-4 py-2 text-slate-700">
-        <i class="py-2 pointer-events-none fa fa-cog"> </i>
-      </a>
-      <!-- -right-90 in loc de 0-->
-      <div fixed-plugin-card class="z-sticky shadow-soft-3xl w-90 ease-soft -right-90 fixed top-0 left-auto flex h-full min-w-0 flex-col break-words rounded-none border-0 bg-white bg-clip-border px-2.5 duration-200">
-        <div class="px-6 pt-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
-          <div class="float-left">
-            <h5 class="mt-4 mb-0">{{ env('APP_NAME') }}</h5>
-            <p>See our dashboard options.</p>
-          </div>
-          <div class="float-right mt-6">
-            <button fixed-plugin-close-button class="inline-block p-0 mb-4 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer hover:scale-102 leading-pro text-xs ease-soft-in tracking-tight-soft bg-150 bg-x-25 active:opacity-85 text-slate-700">
-              <i class="fa fa-close"></i>
-            </button>
-          </div>
-          <!-- End Toggle Button -->
-        </div>
-        <hr class="h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-      </div>
-    </div>
   </body>
   <!-- plugin for charts  -->
   <script src="{{ asset('/assets/js/plugins/chartjs.min.js') }}" async></script>
